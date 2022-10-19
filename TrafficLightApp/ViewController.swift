1687e97d40215dfe975231081bcc81c9dce494db
Evrevolt <gennadi_wow@mail.ru>
2022-10-19 13:16:32 +0300

    Edit constrates and delete Stack

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
+        
+        //Get radius view
+        //redView.layer.cornerRadius = 50
+        //yellowView.layer.cornerRadius = 70
+        //greenView.layer.cornerRadius = 60
+        
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButtonAction(_ sender: UIButton) {
+        
+    }
+    
 }
 
